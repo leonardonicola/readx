dd0b971758cdebf86c968cced7b0dfb3f8c5a59f
Leonardo Nicola <leonardonicolares@gmail.com>
2024-08-22 19:31:50 -0300

    fix: style

--- components/base/template.tsx
+++ components/base/template.tsx
@@ -5,10 +5,12 @@ import Navbar from "./navbar";
 
 export default function BaseTemplate(props: { children: React.ReactNode }) {
   return (
-    <div className="min-h-dvh w-dvw space-y-4">
-      <Navbar />
-      <main className="mx-auto max-w-screen-lg px-4">{props.children}</main>
-      <Footer />
+    <div className="flex h-dvh w-full flex-col items-center gap-4">
+      <Navbar className="w-full" />
+      <main className="h-full w-full max-w-screen-lg px-4">
+        {props.children}
+      </main>
+      <Footer className="w-full" />
     </div>
   );
 }
