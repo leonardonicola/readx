854b535e62da5874a08cd40163134ff0de1e6743
Leonardo Nicola <leonardonicolares@gmail.com>
2024-08-22 19:36:06 -0300

    feat(shadcn): add input otp animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -53,6 +53,10 @@ const config = {
         heading: "700"
       },
       keyframes: {
+        "caret-blink": {
+          "0%,70%,100%": { opacity: "1" },
+          "20%,50%": { opacity: "0" }
+        },
         "accordion-down": {
           from: { height: "0" },
           to: { height: "var(--radix-accordion-content-height)" }
@@ -63,6 +67,7 @@ const config = {
         }
       },
       animation: {
+        "caret-blink": "caret-blink 1.25s ease-out infinite",
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out"
       }
