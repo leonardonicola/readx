83a1d22384b629d38bc8fefbe5d46e9ec90ad36e
Leonardo Nicola <leonardonicolares@gmail.com>
2024-08-30 13:24:47 -0300

    test: create footer sample test

--- components/base/footer.tsx
+++ components/base/footer.tsx
@@ -3,6 +3,7 @@ import { cn } from "@/lib/utils";
 export default function Footer({ className }: { className?: string }) {
   return (
     <footer
+      data-testid="footer"
       className={cn(
         "sticky top-[100dvh] border-t border-gray-300 py-8 text-center text-sm font-bold",
         className
