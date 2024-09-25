e9367a330ab5b806c416c5694dbaac889b44125c
Leonardo Nicola <leonardonicolares@gmail.com>
2024-09-25 14:37:41 -0300

    feat(util): add dayjs adapters mini utilities

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,22 @@
 import { type ClassValue, clsx } from "clsx";
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+/**
+ * @description Validate with custom dayjs parser
+ */
+export function validateDate(date: string | Date, format: string): boolean {
+  dayjs.extend(customParseFormat);
+  return dayjs(date, format, true).isValid();
+}
+
+export function toISOString(date: string | Date, format?: string): string {
+  return dayjs(date, format).toISOString();
 }
 
 export type DefaultFetchResponse<T> =
