99a70a4b396905ae2530ce58df71dcfc54c5abcd
Leonardo Nicola <leonardonicolares@gmail.com>
2025-04-10 19:50:58 -0300

    feat(prisma): add newest schemas with seeds

---
+++ prisma/migrations/20250410140846_init/migration.sql
@@ -0,0 +1,53 @@
+-- CreateTable
+CREATE TABLE "user" (
+    "id" TEXT NOT NULL,
+    "email" VARCHAR(250) NOT NULL,
+    "phoneNumber" VARCHAR(20),
+    "firstName" VARCHAR(150) NOT NULL,
+    "lastName" VARCHAR(150),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "deleted_at" TIMESTAMP(3),
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "bookshelf" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "book_id" TEXT NOT NULL,
+
+    CONSTRAINT "bookshelf_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "book" (
+    "id" TEXT NOT NULL,
+    "title" VARCHAR(255) NOT NULL,
+    "release_date" TIMESTAMP(3) NOT NULL,
+    "author" VARCHAR(150) NOT NULL,
+    "genre_id" INTEGER NOT NULL,
+
+    CONSTRAINT "book_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "genre" (
+    "id" SERIAL NOT NULL,
+    "name" VARCHAR(50) NOT NULL,
+
+    CONSTRAINT "genre_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
+
+-- AddForeignKey
+ALTER TABLE "bookshelf" ADD CONSTRAINT "bookshelf_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "bookshelf" ADD CONSTRAINT "bookshelf_book_id_fkey" FOREIGN KEY ("book_id") REFERENCES "book"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "book" ADD CONSTRAINT "book_genre_id_fkey" FOREIGN KEY ("genre_id") REFERENCES "genre"("id") ON DELETE NO ACTION ON UPDATE CASCADE;
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,27 +1,69 @@
 import prisma from "@/lib/db";
 import { logger } from "@/lib/logger";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 async function main() {
-  await prisma.user.create({
-    data: {
-      id: "user_2hDMCBi0KJXPmV4avSx5RuFQBgn",
-      email: "leonardonicolares@gmail.com",
-      firstName: "Leonardo",
-      lastName: "Nicola"
-    }
-  }),
-    // Create users
+  // Create multiple genres
+  await prisma.genre.createMany({
+    data: [
+      { id: 1, name: "Ação" },
+      { id: 2, name: "Aventura" },
+      { id: 3, name: "Comédia" },
+      { id: 4, name: "Drama" },
+      { id: 5, name: "Fantasia" },
+      { id: 6, name: "Horror" },
+      { id: 7, name: "Mistério" },
+      { id: 8, name: "Romance" },
+      { id: 9, name: "Sci-Fi" },
+      { id: 10, name: "Thriller" },
+      { id: 11, name: "Suspense" },
+      { id: 12, name: "Biografia" },
+      { id: 13, name: "Automóvel" },
+      { id: 14, name: "Desenvolvimento Pessoal" },
+      { id: 15, name: "Desenvolvimento Profissional" },
+      { id: 16, name: "Programação" },
+      { id: 17, name: "Programação Web" },
+      { id: 18, name: "Programação Mobile" },
+      { id: 19, name: "Programação de Jogos" },
+      { id: 20, name: "Redes" },
+      { id: 21, name: "Sistemas Operacionais" },
+      { id: 22, name: "Banco de Dados" },
+      { id: 23, name: "Segurança" },
+      { id: 24, name: "Anime" },
+      { id: 25, name: "Cartoon" }
+    ],
+    skipDuplicates: true
+  });
 
-    logger.info("Dev Database seeded successfully");
-}
+  logger.info("Database seeded successfully");
 
-if (process.env.NODE_ENV === "development") {
-  main()
-    .catch((e) => {
-      logger.error(e);
-      process.exit(1);
-    })
-    .finally(async () => {
-      await prisma.$disconnect();
+  if (process.env.NODE_ENV === "development") {
+    const id = process.env.DEV_USER_ID || "default_user_id";
+    await prisma.user.upsert({
+      where: {
+        id
+      },
+      update: {
+        email: "leonardonicolares@gmail.com",
+        firstName: "Leonardo",
+        lastName: "Nicola",
+        id
+      },
+      create: {
+        email: "leonardonicolares@gmail.com",
+        firstName: "Leonardo",
+        lastName: "Nicola",
+        id
+      }
     });
+  }
 }
+
+main()
+  .catch((e) => {
+    logger.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
